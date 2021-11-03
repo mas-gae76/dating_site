a8b9e8dc78ff4db77e768ca7e9866b2e1c16becf
mas-gae76 <alex2904.goryachev@yandex.com>
2021-11-03 22:50:59 +0300

    added watermark overlay on avatar

--- date/models.py
+++ date/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from utils import *
-from imagekit import ImageSpec
+from .utils import *
+from PIL import Image
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -9,15 +10,29 @@ class User(AbstractUser):
         MALE = 'М', 'Мужской'
         FEMALE = 'Ж', 'Женский'
         __empty__ = 'Укажите Ваш пол'
-    avatar = models.ImageField(verbose_name='Аватар', blank=True, upload_to=get_timestamp_path_user)
-    gender = models.CharField(verbose_name='Пол', choices=Gender.choices, default=Gender.MALE)
-    name = models.CharField(verbose_name='Имя', max_length=40)
+
+    avatar = models.ImageField(verbose_name='Аватар', default=None, upload_to=get_timestamp_path_user)
+    gender = models.CharField(max_length=1, verbose_name='Пол', choices=Gender.choices, default=Gender.MALE)
+    first_name = models.CharField(verbose_name='Имя', max_length=40)
     last_name = models.CharField(verbose_name='Фамилия', max_length=50)
-    email = models.EmailField(max_length=128)
-    password = models.CharField(max_length=25)
+    email = models.EmailField(max_length=128, unique=True)
+    username = models.CharField(blank=True, max_length=10)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        super().save()
+        if self.avatar:
+            avatar = Image.open(self.avatar.path)
+            watermark = Image.open(settings.WATERMARK_PATH)
+            watermark.thumbnail((250, 250))
+            x = avatar.size[0] - watermark.size[0] - 20
+            y = avatar.size[1] - watermark.size[1] - 20
+            avatar.paste(watermark, (x, y))
+            avatar.save(self.avatar.path)
 
     def __str__(self):
-        return f'{self.name} {self.last_name}'
+        return f'{self.email} - {self.first_name} {self.last_name}'
 
     class Meta:
         verbose_name = 'Участник'
